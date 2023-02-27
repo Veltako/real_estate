678f5bccb22339204daf0a678592e6c5f303e71a
Veltako <baptistedupache@gmail.com>
2023-02-27 10:39:29 +0100

    new js

---
+++ js/button.js
@@ -0,0 +1,8 @@
+let btn = document.getElementById('btn');
+
+
+document.getElementById('btn').onclick = function() {
+btn.textContent = 'Au 00 65 87 99 00';
+btn.style.transform = "width: 150px;"
+
+}
