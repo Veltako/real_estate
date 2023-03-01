b23c46a255e08da733a4e5fa8c4ebb3e767420ba
Veltako <baptistedupache@gmail.com>
2023-03-01 11:35:20 +0100

    tttt

--- js/button.js
+++ js/button.js
@@ -1,8 +1,8 @@
-let btn = document.getElementById('btn');
-
-
-document.getElementById('btn').onclick = function() {
-btn.textContent = 'Au 00 65 87 99 00';
-btn.style.transform = "width: 150px;"
-
+function btn() {
+    var btn = document.getElementById("btn");
+    if (btn.innerHTML === "Contactez") {
+        btn.innerHTML = "Au 00 65 87 99 00";
+    } else {
+        btn.innerHTML = "Contactez";
+    }
 }
